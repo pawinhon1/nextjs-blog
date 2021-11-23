ebaac2e43b80738bd216d9bb8cb6a929c708f2a5
Pawin <pawin@cpru.ac.th>
2021-11-23 15:08:25 +0700

    nextjs first app

--- MyFirstNextJS/nextjs-blog/pages/layout.js
+++ MyFirstNextJS/nextjs-blog/pages/layout.js
@@ -1,4 +1,4 @@
-import styles from '../../components/layout.module.css'
+import styles from '../components/layout.module.css'
 
 export default function Layout({children}){
     return <div class={styles.container}>{children}</div>
